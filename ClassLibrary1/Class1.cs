5fb7ba94468bfb54300f53862535ba02c5d6a382
Тарасенко Евгений Викторович <tarasenko.ev1644@okeit.edu>
2023-04-24 10:28:24 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculation
+{
+    static public class CalculationLib
+    {
+        static public double Plus(double a, double b)
+        {
+            return a + b;
+        }
+        static public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+        static public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+        static public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Calculation;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculationLib.Plus(10, 5));
+            Console.WriteLine(CalculationLib.Minus(10, 5));
+            Console.WriteLine(CalculationLib.Divide(10, 5));
+            Console.WriteLine(CalculationLib.Multiply(10, 5));
+        }
+    }
+}
